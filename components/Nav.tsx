2865c14518eb3cf94df4c559e3a733c1d3d418b3
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2023-06-15 15:57:11 +0200

    Added more Navigation and Login Page Integration

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import LoginPanel from "./LoginPanel";
 import {
   signIn,
   signOut,
@@ -22,61 +23,84 @@ const Nav = () => {
     };
     fetchProviders();
   });
-
+  function openLoginPanel() {
+    const login_dialog = document.getElementById("login_dialog");
+    login_dialog.showModal();
+  }
   return (
-    <nav className="navbar mb-16 pt-3">
-      <div className="navbar-start">
-        <Link href="/" className="flex gap-2 flex-center items-center">
-          <Image
-            src="/assets/images/Prepify_logo.png"
-            alt="Prepify Logo"
-            width={50}
-            height={50}
-          />
-          <p>Prepify</p>
-        </Link>
-      </div>
-      <div className="navbar-center">
-        
-      </div>
-      <div className="navbar-end sm:flex hidden p-5">
-        {session?.user ? (
-          <div className="dropdown dropdown-hover">
-            <Link href="/profile">
-              <div className="flex gap-3 md:gap-5">
-                {session?.user.username}
-                <Image
-                  src={session?.user.image as string}
-                  width={30}
-                  height={30}
-                  alt="ProfilePicture"
-                />
-              </div>
+    <>
+      <nav className="navbar mb-16 pt-3">
+        <div className="navbar-start">
+          <Link href="/" className="flex gap-2 flex-center items-center">
+            <Image
+              src="/assets/images/Prepify_logo.png"
+              alt="Prepify Logo"
+              width={50}
+              height={50}
+            />
+            <p>Prepify</p>
+          </Link>
+        </div>
+        <div className="navbar-center">
+          <div className="flex space-x-4">
+            <Link href="/weekly_recipe">
+              <p>Weekly Recipe</p>
             </Link>
-            <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-              <li>
-                <Link href="/profile">Your Profile</Link>
-              </li>
-              <li>
-                <button className="btn btn-secondary" onClick={() => signOut()}>
-                  Log out
+            <Link href="/step_by_step">
+              <p>Guide</p>
+            </Link>
+            <Link href="/notion_setup">
+              <p>Setup Notion</p>
+            </Link>
+          </div>
+        </div>
+        <div className="navbar-end sm:flex hidden p-5">
+          {session?.user ? (
+            <div className="dropdown dropdown-hover">
+              <Link href="/profile">
+                <div className="flex gap-3 md:gap-5">
+                  {session?.user.username}
+                  <Image
+                    src={session?.user.image as string}
+                    width={40}
+                    height={40}
+                    alt="ProfilePicture"
+                    className="rounded-full"
+                  />
+                </div>
+              </Link>
+              <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                  <Link href="/profile">Your Profile</Link>
+                </li>
+                <li>
+                  <button className="" onClick={() => signOut()}>
+                    Log out
+                  </button>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div>
+              {provider ? (
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => openLoginPanel()}
+                >
+                  Sign in now!
                 </button>
-              </li>
-            </ul>
-          </div>
-        ) : (
-          <div>
-            {provider ? (
-              <button type="button" className="btn btn-primary">
-                Sign in now!
-              </button>
-            ) : (
-              <></>
-            )}
-          </div>
-        )}
-      </div>
-    </nav>
+              ) : (
+                <></>
+              )}
+            </div>
+          )}
+        </div>
+      </nav>
+      <dialog id="login_dialog">
+        <LoginPanel />
+      </dialog>
+    </>
   );
 };
 
