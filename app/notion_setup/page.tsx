1d7a545bbc0aaa1960e5333c6b217c72b6cdf50b
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2023-11-29 16:18:40 +0100

    added notion secret setup

--- app/notion_setup/page.tsx
+++ app/notion_setup/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
-import { step_by_step_guide_response } from "../global_types/meal";
 import Image from "next/image";
-export default class Notion_setup extends React.Component<
-  {},
-  step_by_step_guide_response
-> {
+export default class Notion_setup extends React.Component<{}> {
+  constructor({}) {
+    super({});
+    this.sendIngredientList = this.sendIngredientList.bind(this);
+  }
+  sendIngredientList = () => {
+    alert("yiyi");
+  };
   render() {
     return (
       <>
@@ -22,56 +25,109 @@ export default class Notion_setup extends React.Component<
           </div>
 
           <h2>How to setup Notion?</h2>
-          <div>
+          <div className="flex flex-col justify-center items-center prose max-w-full">
             <h3>Create an Integration</h3>
             <p>
-              To setup Notion you first need to head to your notion integrations{" "}
-              <a href="https://www.notion.so/my-integrations">
-                My Integrations
-              </a>
+              To setup Notion you first need to head to your notion{" "}
+              <a href="https://www.notion.so/my-integrations">Integrations</a>
             </p>
 
             <p>Press on New Integration</p>
             <Image
-              src={""}
-              width={20}
-              height={20}
+              src={"/assets/images/NotionSetupPictures/newIntegration.png"}
+              width={1000}
+              height={1000}
               alt="New Integration Button"
+              className="w-1/2"
             />
             <p>Select your desired Workspace and a fitting name</p>
             <p>
               And you can also upload a logo if you want. The Prepify Logo can
-              be found here:
+              be found <a>here</a>
             </p>
-            <a>Link zum Prepify Logo Download</a>
 
-            <p>Click on Send</p>
-            <Image alt="Image of send button" src={""}></Image>
-
-            <p>Check the Security Tab and enable following features:</p>
-            <Image alt="Image of Zugriffsrechte" src=""></Image>
-            <p>Go to Secret Tab and copy the secret</p>
-
+            <p>Click on Submit</p>
+            <Image
+              alt="Image of submit button"
+              src={"/assets/images/NotionSetupPictures/integrationSubmit.png"}
+              width={1000}
+              height={1000}
+              className="w-1/2"
+            />
             <p>
-              Now go to the Page where you want the Shoppinglist to be saved
+              You will be redirected to the secret tab, copy your secret to your
+              clipboard.
+            </p>
+
+            <h3>Capabilities Settings</h3>
+            <p>Go to the Capabilities Tab and enable following features:</p>
+            <Image
+              alt="Image of Capabilities"
+              src="/assets/images/NotionSetupPictures/capabilitiesSettings.png"
+              width={1000}
+              height={1000}
+              className="w-1/2"
+            ></Image>
+            <h3>Setup Shoppinglist</h3>
+            <p>
+              In Notion go to the Page where you want the Shoppinglist to be
+              saved or create a new one Called "Prepify Shoppinglist"
             </p>
             <p>
               Press the three dots on top and go to connections Add Connection
               Select your created Integration
             </p>
+            <Image
+              alt="Image of Shopping Page"
+              src="/assets/images/NotionSetupPictures/setupShoppingList.png"
+              width={1000}
+              height={1000}
+              className="w-1/2"
+            />
 
-            <p>Now go to your Profile</p>
-            <a>Link to your Profile</a>
+            <p>And accept the access</p>
+            <Image
+              alt="Image of accepting the access"
+              src="/assets/images/NotionSetupPictures/acceptAccess.png"
+              width={1000}
+              height={1000}
+              className="w-1/2"
+            />
 
-            <p>Go to general settings</p>
+            <h3>Set Secret in Prepify Profile</h3>
+
+            <p>
+              Now go to your{" "}
+              <a href="/profile" target="_blank">
+                Profile{" "}
+              </a>
+              in the general settings
+            </p>
+
             <p>Add your Secret in the textbox and press save settings</p>
-
+            <Image
+              alt="Image to insert Secret"
+              src="/assets/images/NotionSetupPictures/setSecret.png"
+              width={1000}
+              height={1000}
+              className="w-1/2"
+            />
+            <p>Save your Profile settings!</p>
+            <Image
+              alt="Image to save profile settings"
+              src="/assets/images/NotionSetupPictures/saveSettings.png"
+              width={1000}
+              height={1000}
+              className="w-1/2"
+            />
             <p>
               Now Notion should be setup! You can try it by pressing the button
               over here or wait till friday!
             </p>
 
-            <button>Press to add the ingredients to your shoppinglist!</button>
+            <button onClick={() => this.sendIngredientList()}>
+              Press to add the ingredients to your shoppinglist!
+            </button>
           </div>
         </div>
       </>
